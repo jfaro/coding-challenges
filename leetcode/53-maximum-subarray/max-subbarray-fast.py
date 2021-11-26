3b15388023f912e9527418549875546aced48947
jfaro <jfaro@umich.edu>
2021-11-25 23:08:10 -0500

    Update LeetCode problem #53 (Maximum Subarray)

--- leetcode/53-maximum-subarray/max-subbarray-fast.py
+++ leetcode/53-maximum-subarray/max-subbarray-fast.py
@@ -19,5 +19,3 @@ solution = Solution()
 print(solution.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))    # 6
 print(solution.maxSubArray([1]))                                # 1
 print(solution.maxSubArray([5, 4, -1, 7, 8]))                   # 23
-print(solution.maxSubArray([-57, 9, -72, -72, -62, 45, -97, 24, -39, 35, -
-                            82, -4, -63, 1, -93, 42, 44, 1, -75, -25, -87, -16, 9, -59, 20, 5, -95]))
--- leetcode/53-maximum-subarray/max-subbarray.py
+++ leetcode/53-maximum-subarray/max-subbarray.py
@@ -1,26 +1,3 @@
-# Given an integer array nums, find the contiguous subarray (containing at least one number)
-# which has the largest sum and return its sum.
-
-# A subarray is a contiguous part of an array.
-
-# Example 1:
-# Input: nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-# Output: 6
-# Explanation: [4, -1, 2, 1] has the largest sum = 6.
-
-# Example 2:
-# Input: nums = [1]
-# Output: 1
-
-# Example 3:
-# Input: nums = [5, 4, -1, 7, 8]
-# Output: 23
-
-# Constraints:
-# 1 <= nums.length <= 105
-# -104 <= nums[i] <= 104
-
-
 class Solution(object):
     def maxSubArray(self, nums):
         """
@@ -58,5 +35,3 @@ solution = Solution()
 print(solution.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))    # 6
 print(solution.maxSubArray([1]))                                # 1
 print(solution.maxSubArray([5, 4, -1, 7, 8]))                   # 23
-print(solution.maxSubArray([-57, 9, -72, -72, -62, 45, -97, 24, -39, 35, -
-                            82, -4, -63, 1, -93, 42, 44, 1, -75, -25, -87, -16, 9, -59, 20, 5, -95]))
